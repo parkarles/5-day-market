31a1f9b1ef2ad5d55d16c4387125ac4421a3eedb
gngsn <rudtjs4540@gmail.com>
2021-08-05 20:45:01 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/triple-slash-reference": 0,
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off"
+  },
+};
